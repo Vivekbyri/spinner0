879bc54a7dbc0dcae3778c4876485d0916894326
Vivekbyri <vivek.byri@springml.com>
2020-02-11 14:40:55 +0530

    first commit

---
+++ spinner-element.js
@@ -0,0 +1,33 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-spinner/paper-spinner.js';
+
+/**
+ * `spinner-element`
+ * To display a loader
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class SpinnerElement extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <paper-spinner active></paper-spinner>
+    `;
+  }
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'spinner-element',
+      },
+    };
+  }
+}
+
+window.customElements.define('spinner-element', SpinnerElement);
